80f21b3554429876ed3a51005c978938d7fcaf89
Ravi <ravi2krishna@gmail.com>
2019-05-05 17:41:43 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,3 +1,5 @@
 import subprocess
+import urllib
+urllib.urlretrieve("https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4", filename="/home/ec2-user/video.mp4")
 subprocess.call(["aws", "s3", "mb", "s3://aws-lab-videos-your-name","--region","us-east-2"])
 subprocess.call(["aws", "s3", "cp", "video.mp4","s3://aws-lab-videos-your-name/video.mp4"])
