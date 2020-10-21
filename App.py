dcdb4a758496eb8302c9f64b170616b364239311
Ravi <ravi2krishna@gmail.com>
2020-10-21 08:43:08 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 import subprocess
 import urllib
-urllib.urlretrieve("https://file-examples.com/wp-content/uploads/2017/04/file_example_MP4_480_1_5MG.mp4", filename="/home/ec2-user/video.mp4")
-subprocess.call(["aws", "s3", "mb", "s3://aws-lab-videos-your-name","--region","us-east-2"])
-subprocess.call(["aws", "s3", "cp", "/home/ec2-user/video.mp4","s3://aws-lab-videos-your-name/video.mp4"])
+urllib.urlretrieve("https://www.w3.org/TR/PNG/iso_8859-1.txt", filename="/home/ec2-user/sample.txt")
+subprocess.call(["aws", "s3", "mb", "s3://aws-lab-unique-bucket-name","--region","us-east-2"])
+subprocess.call(["aws", "s3", "cp", "/home/ec2-user/sample.txt","s3://aws-lab-unique-bucket-name/sample.txt"])
